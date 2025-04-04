3d1df1e2c86be37ab0f7dc79ff7a5cca3d911ced
Joel050505 <Joel.kilic@chasacademy.se>
2025-04-04 15:02:14 +0200

    test

---
+++ Frontend/components/Footer.jsx
@@ -0,0 +1,10 @@
+"use client";
+import { useState } from "react";
+
+export default function Footer() {
+  return (
+    <footer className="text-center p-4 w-full">
+      <p>&copy; 2025 Joel & Daniel. All rights reserved.</p>
+    </footer>
+  );
+}
